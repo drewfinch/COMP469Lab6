e45e80e26cb2aa978eac96817d5248617fa0f8c6
drewfinch <35159825+drewfinch@users.noreply.github.com>
2021-09-29 10:12:50 -0700

    some more functions

--- main.py
+++ main.py
@@ -5,6 +5,8 @@
 # 3. The game is over when there are no more numbers left in the list.
 # 4. Winner: the player with more collected points
 # 5. Min and Max both play optimally
+INFINITY = 100000000000
+
 
 class Node:
     def __init__(self, data, parent):
@@ -12,13 +14,21 @@ class Node:
         self.children = []
         self.data = data
 
+def get_successors(tree):
+    children = []
+    return children
+
 
 def max(numbers):
-    return
+    v = -INFINITY
+
+    for each successor of state:
+        v = max(v, min - value(successor))
+    return v
 
 
 def min(numbers):
-    return
+    return 0
 
 
 def print_winner(min_score, max_score):
@@ -30,7 +40,7 @@ min_score = 0
 max_score = 0
 
 while len(numbers):
-    max(numbers)
-    min(numbers)
+    max_score = max_score + max(numbers)
+    min_score = min_score + min(numbers)
 
 print_winner(min_score, max_score)
