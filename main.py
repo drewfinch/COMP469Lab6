c08bf2e758101132e040f0f08d68cdfae6d49a7a
CSUCIkoxu <70290605+CSUCIkoxu@users.noreply.github.com>
2021-09-29 11:57:08 -0700

    Added drivers
    Added the driver function (play) to the program to run max and min
    
    Added input scanning
    
    Added the output

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # @authors: Christopher Chang, Danial Thai, Drew Finch
 
+# Minimax Game:
 # Two players (Max, Min) are given a list of numbers, e.g. [1,2,5,2]
 # 1. The players take turn in picking a number from either the beginning or the end of the
 # list.
@@ -8,6 +9,8 @@
 # 4. Winner: the player with more collected points
 # 5. Min and Max both play optimally
 INFINITY = 100000000000
+
+import copy
 
 
 class Node:
@@ -20,28 +23,99 @@ def get_successors(tree):
     children = []
     return children
 
+def isEmpty(numbers):
+    return len(numbers) == 0
 
 ### The max and min functions cannot be called max or min because they are reserved functions!
 def maximize(numbers):
     v = -INFINITY
 
-    for each successor of state:
-        v = max(v, min - value(successor))
+    # for each successor of state:
+    #     v = max(v, min - value(successor))
     return v
 
 
 def minimize(numbers):
     return 0
 
+#driver code that runs minimize and maximize
+def play(numbers):
+    numCpy = copy.deepcopy(numbers)
+    maxScore = 0
+    minScore = 0
+    
+    gameEnded = isEmpty(numbers)
+    
+    while (not gameEnded):
+        maxChoice = maximize(numCpy)            #asserts that that maximize will return the choice branch that will make max win
+        if (maxChoice == numCpy[0]):    #numbers is automatically decreased each choice, just worry about getting the ideal path from a given state of numbers
+            numCpy = numCpy[1:]
+        else:
+            numCpy = numCpy[:len(numCpy)-1]
+        maxScore += maxChoice
+        
+        gameEnded = isEmpty(numCpy)
+        
+        if (not gameEnded):
+            minChoice = minimize(numCpy)        #asserts that minimize will return the choice branch that will make max lose
+            if (minChoice == numCpy[0]):
+                numCpy = numCpy[1:]
+            else:
+                numCpy = numCpy[:len(numCpy)-1]
+            minScore += minChoice
+            
+        gameEnded = isEmpty(numCpy)
+        
+        return (maxScore, -minScore)
 
-def checkWinner(maxScore, minScore)
+def checkWinner(maxScore, minScore):
+    maxWon = False
+    tie = False
+    scoreSum = maxScore + minScore
+    
+    if (scoreSum > 0):
+        maxWon = True
+    elif (scoreSum == 0):
+        tie = True
+    
+    return (maxWon, tie)
+
+#Start Misc. Functions --------------------------------------------------------
+def scanFile(file, numbers):
+    data = file.read().replace(' ', '').split(",")
+        
+    numbers = list(map(int, data))
+    
+    return numbers
+
+def scanInput(strIn, numbers):
+    data = strIn.replace(' ', '').split(",")
+    
+    numbers = list(map(int, data))
+    
+    return numbers
+#End Misc. Functions ----------------------------------------------------------
+
 
 numbers = [1, 2, 5, 2]
-min_score = 0
-max_score = 0
 
-while len(numbers):
-    max_score = max_score + max(numbers)
-    min_score = min_score + min(numbers)
+##uncomment these lines of code to test different input
+option = input("manuel or file input?\n")
 
-print_winner(min_score, max_score)
+if (option.lower() == "file"):
+    print("Give a file path to a list of numbers\nThe numbers should be seperated with commas")
+    input("File Path:\n")
+else:
+    print("Give a list of numbers\nThe numbers should be seperated with commas")
+    input("List:\n")
+    
+scores = play(numbers) #(max_score, min_score)
+
+winState = checkWinner(scores[0], scores[1])
+
+if (winState[0]):
+    print("The maximizer won")
+elif (winState[1]):
+    print("Tie!")
+else:
+    print("The maximizer lost")
